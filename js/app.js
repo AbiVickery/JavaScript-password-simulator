7e7ddbaf0bfd657ab60016c2a9a83c6c6c3fd9db
Abi Vickery <vickeryabi@gmail.com>
2019-11-05 14:29:02 -0600

    notes, not quite psuedo code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-document.write('Hello world!');
+// document.write('Hello world!');
+
+// Password must have at least 8 characters but no more than 20;
+// Must contain at least one special charcter(!, @, #, $, %, &);
+// Must contain at least one uppercase letter;
+// Must contain at least one lowercase letter;
+// Must contain at least one number;
+// cannot contain spaces;
