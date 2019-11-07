96101e36d0eedc6381ae9b2b67e40411c5b1ae5a
Abi Vickery <vickeryabi@gmail.com>
2019-11-07 09:48:28 -0600

    psuedo code

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@
 // Must contain at least one lowercase letter;
 // Must contain at least one number;
 // cannot contain spaces;
+
+/* first, i start out with the psuedo code */
+// a function for the box to enter the password into // targeting a div id from the html
+// a function to tell if the password contains at least one special character(!, @, #, $, %, &)
+// a function to tell if the password contains at least one uppercase letter
+// a function to tell if the password contains at least one lowercase letter
+// a function to tell if the passwrod contains at least one number
+// a function to tell if the password contains zero spaces
