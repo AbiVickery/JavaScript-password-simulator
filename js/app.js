0ce63be78a8aa0ef3958313ae8e72e9a69fa0c34
Abi Vickery <vickeryabi@gmail.com>
2019-11-08 11:11:09 -0600

    javascript
    and lots of it

--- js/app.js
+++ js/app.js
@@ -21,3 +21,34 @@
 //     let no = false;
 //     if ()
 // };
+
+var code = document.getElementById('code');
+code.addEventListener('keyup', checkcode);
+
+
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var space = [' '];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_'];
+
+function checkCode(code) {
+    code = event.target.value ? event.target.value : code;
+
+    let rules = (
+        length: checkLength(code),
+        char: checkChar(code),
+        upper: checkUpperLower(code),
+        lower: checkUpperLower(code, false),
+        number: checkNumber(code),
+        space: checkSpace(code)
+    )
+    // let tests = document.querySelectorAll('.test');
+
+    return rules.length
+           && rules.char
+           && rules.upper
+           && rules.lower
+           && rules.number
+           && rules.space;
+}
